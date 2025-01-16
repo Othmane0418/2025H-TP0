0077413f176371d2563d1f08fb87dc0fb9b3dc3c
Othmane0418 <othmane.harbi@polymtl.ca>
2025-01-16 15:37:14 -0500

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+aire = 3.14*rayon**2
+print(aire)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print('Othmane Harbi')
